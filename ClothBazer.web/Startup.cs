26da5c2b9a60cfb338f492325b289699a1ab00db
edgarmurudu@gmail.com <edgarmurudu@gmail.com>
2020-11-04 12:52:15 +0200

    Add project files.

---
+++ ClothBazer.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClothBazer.web.Startup))]
+namespace ClothBazer.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
